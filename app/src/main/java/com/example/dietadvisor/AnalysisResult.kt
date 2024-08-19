42eeba41265dcd3209539a6f9ae24f3d1dddd01a
chitsip149 <minhtrangkl03@gmail.com>
2024-08-19 23:36:16 +0800

    added new files

---
+++ app/src/main/java/com/example/dietadvisor/AnalysisResult.kt
@@ -0,0 +1,21 @@
+package com.example.dietadvisor
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class AnalysisResult : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        getSupportActionBar()?.hide()
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_analysis_result)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
---
+++ app/src/main/java/com/example/dietadvisor/RecognitionResult.kt
@@ -0,0 +1,21 @@
+package com.example.dietadvisor
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class RecognitionResult : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        getSupportActionBar()?.hide()
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_recognition_result)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
